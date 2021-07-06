0f4b065ba8f37f61a731109ee50c090c3ab1922d
Najib <najib.lattouf17@gmail.com>
2021-07-06 21:30:56 +0300

    final

---
+++ index.js
@@ -0,0 +1,22 @@
+fetch('https://najib-vlava.herokuapp.com/server')
+  .then(response => response.json())
+  .then(json => {
+var services = document.getElementById('services')
+var content = `<div>
+                <div class="icon"><i class="bi bi-briefcase"></i></div>
+                <h4 class="title"><a href="">Lorem Ipsum</a></h4>
+                <p class="description">Voluptatum</p>
+                </div>`
+
+                for (let i = 0; i < json.length; i++) {
+                    var specificContent = content.replace("Lorem Ipsum",json[i].Services);
+                    specificContent = specificContent.replace('Voluptatum',json[i].Description);
+                    var singleservice = document.createElement('div')
+                    singleservice.className = "col-lg-4 col-md-6 icon-box";
+                    singleservice.innerHTML = specificContent;
+                    services.appendChild(singleservice);
+                }
+
+
+
+  })
