adeb55911b4ff383cf00b9e2f13ee420553fb96c
Najib <najib.lattouf17@gmail.com>
2021-07-06 19:33:29 +0300

    first commit

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+app.use(cors())
+ 
+app.get('/Server', function (req, res) {
+    // Requiring the module
+    const reader = require('xlsx')
+      
+    // Reading our test file
+    const file = reader.readFile('./data.xlsx')
+      
+    let data = []
+      
+    const sheets = file.SheetNames
+      
+    for(let i = 0; i < sheets.length; i++)
+    {
+       const temp = reader.utils.sheet_to_json(
+            file.Sheets[file.SheetNames[i]])
+       temp.forEach((res) => {
+          data.push(res)
+       })
+    }
+      res.send(data)
+    })
+    
+     
+    app.listen(1234)
